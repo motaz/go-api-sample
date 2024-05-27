3bf55768a86601e107f6aacca25af8623f8cabd7
motaz <mutaz@code.sd>
2024-05-27 07:27:53 +0200

    display listening port

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ func main() {
 	http.HandleFunc("/addorder", AddOrder)
 	http.HandleFunc("/removeorder", RemoveOrder)
 	http.HandleFunc("/getorders", GetOrders)
+	fmt.Println("http://localhost:8888")
 	err := http.ListenAndServe(":8888", nil)
 	if err != nil {
 		fmt.Println(err.Error())
